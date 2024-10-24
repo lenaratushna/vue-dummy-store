fc5d59653ebf498127ff86131e81e9465942cf01
Olena Ratushna <olena.ratushna21@gmail.com>
2024-10-24 22:18:09 +0300

    Fix images

--- src/components/HomeSlider.vue
+++ src/components/HomeSlider.vue
@@ -13,17 +13,17 @@ const modules = [Pagination, Autoplay]
 const slides = [
   {
     slug: 'mobile-accessories',
-    imgSrc: '/vue-dummy-store/assets/images/mobile-accessories.jpg',
+    imgSrc: '/images/mobile-accessories.jpg',
     title: 'Mobile Must-Haves',
   },
   {
     slug: 'sunglasses',
-    imgSrc: '/vue-dummy-store/assets/images/sunglasses.jpg',
+    imgSrc: '/images/sunglasses.jpg',
     title: 'Stylish Sunglasses',
   },
   {
     slug: 'fragrances',
-    imgSrc: '/vue-dummy-store/assets/images/fragrances.jpg',
+    imgSrc: '/images/fragrances.jpg',
     title: 'Essence of Fragrance',
   },
 ]
